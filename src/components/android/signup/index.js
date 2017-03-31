dbdb53f71158e4a60cd732b5d56410f0aa50ed0e
Deuanz <rattanawadi@proteus-tech.com>
2017-03-31 23:14:50 +0700

    add comfirm password

--- src/components/android/signup/index.js
+++ src/components/android/signup/index.js
@@ -12,17 +12,21 @@ const SignUp = (props) => {
       </Header>
       <Content style={{paddingLeft: 20, paddingRight: 20}}>
         <Thumbnail 
-          style={{ alignSelf: 'center', marginTop: 50, marginBottom: 10, width: 100, height: 100 }} 
+          style={{ alignSelf: 'center', marginTop: 30, marginBottom: 10, width: 100, height: 100 }} 
           source={require('./img/logo.png')} 
         />
         <Text style={{ alignSelf: 'center'}}>Create an account</Text>
-        <Form style={{paddingLeft: 20, paddingRight: 20, marginTop: 25}}>
+        <Form style={{paddingLeft: 20, paddingRight: 20, marginTop: 20}}>
           <Item floatingLabel last>
             <Label>E-mail</Label>
             <Input />
           </Item>
           <Item floatingLabel last>
             <Label>Password</Label>
+            <Input secureTextEntry />
+          </Item>
+          <Item floatingLabel last>
+            <Label>Confirm password</Label>
             <Input secureTextEntry />
           </Item>
           <Button block rounded info style={{marginTop: 20, marginBottom: 5 }}>
