40a132a5bb9ba77b7603075adc1bf49216add273
Deuanz <rattanawadi@proteus-tech.com>
2017-03-31 22:06:48 +0700

    Set style

--- src/components/android/signin/index.js
+++ src/components/android/signin/index.js
@@ -10,22 +10,22 @@ const SignIn = (props) => {
           <Title>{title}</Title>
         </Body>
       </Header>
-      <Thumbnail style={{ alignSelf: 'center', marginTop: 50, marginBottom: 30 }} source={require('./logo.png')} />
+      <Thumbnail style={{ alignSelf: 'center', marginTop: 70, marginBottom: 50, width: 150, height: 150 }} source={require('./img/logo.png')} />
       <Content>
-        <Form style={{paddingLeft: 10, paddingRight: 10}}>
+        <Form style={{paddingLeft: 20, paddingRight: 20}}>
           <Item>
             <Label>E-mail</Label>
             <Input />
           </Item>
           <Item>
             <Label>Password</Label>
-            <Input />
+            <Input secureTextEntry />
           </Item>
           <Button block rounded info style={{marginTop: 20, marginBottom: 5 }}>
             <Text>Sign In</Text>
           </Button>
           <Button transparent success style={{alignSelf: 'center'}}>
-            <Text>Register</Text>
+            <Text>Create a new account</Text>
           </Button>
         </Form>
       </Content>
