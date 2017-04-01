6c6df84591ebeaf4792b5769dee5fa56242f404e
sakgunner <sakgunner@gmail.com>
2017-04-01 17:20:30 +0700

    added react navigation

--- src/android/screens/HomeScreen.js
+++ src/android/screens/HomeScreen.js
@@ -1,24 +1,12 @@
 import React from 'react'
-import {View, Button} from 'react-native'
 import Home from '../components/home'
 
-class HomeScreen extends React.PureComponent {
-  static navigationOptions = {
-    title: 'RoamMeet'
-  }
+const HomeScreen = () => (
+  <Home title="RoamMeet" />
+)
 
-  render() {
-    const { navigate } = this.props.navigation
-    return (
-      <View>
-        <Home title="RoamMeet" />
-        <Button
-          onPress={() => navigate('SignIn')}
-          title="Sign In"
-        />
-      </View>
-    )
-  }
+HomeScreen.navigationOptions = {
+  title: 'HomeScreen'
 }
 
 export default HomeScreen
