e3db80fa45f95adb7e59b619fa6d19e6bbac4105
ifferrie <saowaluk.jir@gmail.com>
2017-04-01 21:58:49 +0700

    mock listMeetup

--- src/ios/components/EventList.js
+++ src/ios/components/EventList.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import {Content, Card, CardItem, Body, Text} from 'native-base'
 import EventListItem from './EventListItem'
+import {listMeetup} from '../../meetup'
 
-const eventList = [
-  {
-    inviter: 'Somjai',
-    date: '10-2-2017',
-    description: 'ma ter yhak jer :-)',
-    title: 'Reunion Angel gang xoxo.',
-    pic:'./butter.jpg'
-  },
-  {
-    inviter: 'SomChai',
-    date: '10-2-2017',
-    description: 'kidtueng jung ma ha nhoi <3',
-    title: 'Tee derm',
-    pic:'./butter.jpg'
+class EventList extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      eventList: []
+    }
   }
-]
-const EventList = () => {
-  return (
-    <Content>
-      {
-        eventList.map((event, index) => <EventListItem detail={event} key={index}/>)
-      }
-    </Content>
-  )
+
+  componentDidMount () {
+    listMeetup()
+      .then ((eventList) => {
+        this.setState({eventList})
+      })
+  }
+
+  render () {
+    return (
+      <Content>
+        {
+          this.state.eventList.map((event, index) => <EventListItem detail={event} key={index}/>)
+        }
+      </Content>
+    )
+  }
+
 }
 
 export default EventList
