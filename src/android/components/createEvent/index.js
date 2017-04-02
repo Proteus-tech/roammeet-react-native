96fd2e69b14656c6ff7c3d1821fc52b3787698ab
Deuanz <rattanawadi@proteus-tech.com>
2017-04-02 15:08:09 +0700

    change date picker to use from react-native-datepicker

--- src/android/components/createEvent/index.js
+++ src/android/components/createEvent/index.js
@@ -3,41 +3,13 @@ import colors from '../../../colors'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {Container, Text, Form, Item, Label, Input, Button, Content } from 'native-base'
 import {DatePickerAndroid} from 'react-native'
+import DatePicker from 'react-native-datepicker'
 
 class CreateEvent extends React.Component {
-  state = {
-    presetDate: new Date(2020, 4, 5),
-    simpleDate: new Date(2020, 4, 5),
-    spinnerDate: new Date(2020, 4, 5),
-    calendarDate: new Date(2020, 4, 5),
-    defaultDate: new Date(2020, 4, 5),
-    allDate: new Date(2020, 4, 5),
-    simpleText: 'pick a date',
-    spinnerText: 'pick a date',
-    calendarText: 'pick a date',
-    defaultText: 'pick a date',
-    minText: 'pick a date, no earlier than today',
-    maxText: 'pick a date, no later than today',
-    presetText: 'pick a date, preset to 2020/5/5',
-    allText: 'pick a date between 2020/5/1 and 2020/5/10',
-  };
-
-  showPicker = async (stateKey, options) => {
-    try {
-      var newState = {};
-      const {action, year, month, day} = await DatePickerAndroid.open(options);
-      if (action === DatePickerAndroid.dismissedAction) {
-        newState[stateKey + 'Text'] = 'dismissed';
-      } else {
-        var date = new Date(year, month, day);
-        newState[stateKey + 'Text'] = date.toLocaleDateString();
-        newState[stateKey + 'Date'] = date;
-      }
-      this.setState(newState);
-    } catch ({code, message}) {
-      console.warn(`Error in example '${stateKey}': `, message);
-    }
-  };
+  constructor(props) {
+    super(props)
+    this.state = {date: "2017-04-02"}
+  }
 
   render() {
   return (
@@ -54,10 +26,28 @@ class CreateEvent extends React.Component {
             <Input multiline={true} />
           </Item>
           <Item iconLeft
-            style={{marginBottom: 40, marginTop: 40}}
-            onPress={this.showPicker.bind(this, 'spinner', {date: this.state.spinnerDate, mode: 'spinner'})}>
-            <Icon name='calendar' style={{color: colors.lightGrey}}/>
-            <Text style={{paddingLeft: 20, marginBottom: 5}}>{this.state.spinnerText}</Text>
+            style={{marginBottom: 40, marginTop: 40}}>
+            <DatePicker
+              style={{width: 200}}
+              date={this.state.datetime1}
+              mode="datetime"
+              format="YYYY-MM-DD HH:mm"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              customStyles={{
+                dateIcon: {
+                  position: 'absolute',
+                  left: 0,
+                  top: 4,
+                  marginLeft: 0
+                },
+                dateInput: {
+                  marginLeft: 36
+                }
+              }}
+              minuteInterval={10}
+              onDateChange={(datetime) => {this.setState({datetime1: datetime});}}
+            />
           </Item>
           <Button block rounded info style={{marginTop: 20, marginBottom: 5 }}>
             <Text>Create</Text>
