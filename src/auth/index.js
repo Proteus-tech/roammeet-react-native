c4681419359fc98346a2fc28aa79267edc0ec857
boonprakit <boonprakit@proteus-tech.com>
2017-04-02 19:44:29 +0700

    create auth app

---
+++ src/auth/index.js
@@ -0,0 +1,12 @@
+import Auth0 from 'react-native-auth0'
+
+const auth0 = new Auth0('smnodame.auth0.com')
+const client_id = 'ynsH1st6PtA5Ey3DBty58CKJm9RMe6Wp'
+
+const signin = () => {
+    return
+}
+
+export const signup = () => {
+    return 
+}
